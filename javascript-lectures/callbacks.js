aa5852f32ad03fad9733c325c0829e663e49f75a
sidhi-sid <sidhisidh469@gmail.com>
2020-04-13 19:21:28 +0530

    callbacks with errors

--- javascript-lectures/callbacks.js
+++ javascript-lectures/callbacks.js
@@ -1,20 +1,26 @@
 //downloads a file,compresses it and uploads it somewhere else
 function download(url,downloaded){
 	console.log("Downloading from "+url)
+	if(!url.startsWith('http')){
+	 return downloaded(new Error('URL must start with http'))
+	}
      setTimeout(()=>{
      	let savedfile=url.split("/").pop()
      	console.log("Downloaded and saved as "+savedfile)
-     	downloaded(savedfile)
+     	downloaded(null,savedfile)
      },3000)
 
 
 }
 function compress(file,format,compressed){
 	console.log("Compressing file "+file)
+	if(['zip','gzip','7z'].indexOf(format)==-1){ 
+		return compressed(new Error('We only support zip,gzip,7z'))
+	}
 	setTimeout(()=>{
 		let archive=file.split(".")[0]+"."+format
 		console.log("Compressed and saved as "+archive)
-		compressed(archive)
+		compressed(null,archive)
 
 	},2000)
 
@@ -23,17 +29,27 @@ function compress(file,format,compressed){
 }
 function upload(server,file,uploaded){
 	console.log("Uploading "+file+" to "+server)
+	if(!server.startsWith('ftp://')){
+	 return uploaded(new Error('We only upload to FTP servers'))
+	}
 	setTimeout(()=>{
 		let remotepath=server+"/"+file
 		console.log("Uploaded as "+remotepath)
-	    uploaded(remotepath)
+	    uploaded(null,remotepath)
 	},1000)
 
 
 
 }
-download("https://online.codingblocks.com/app/player/66486/content/62623/5717/video?code=jZVnINGN",(file)=>{
-	compress(file,"zip",(archive)=>{
-		upload("https://file.com",archive,()=>{})
+download("https://online.codingblocks.com/app/player/66486/content/62623/5717/video?code=jZVnINGN",(err,file)=>{
+	if(err) throw err
+	compress(file,"zip",(err,archive)=>{
+		if(err){
+			console.warn(err)
+			archive=file
+		}
+		upload("ftp://file.com",archive,(err)=>{
+	    if(err) throw err
 	})
+})
 })
---
+++ strings.js
@@ -0,0 +1,21 @@
+let str="this is sid's string"
+console.log(str);
+let str1="this is\
+... continued\
+... till here"
+console.log(str1);
+console.log(str1.length)
+let s="this has some data here"
+let t="has"
+console.log(s.indexOf(t));
+let t1="have"
+console.log(s.indexOf(t1));
+ console.log(s.indexOf("er"));
+ console.log(s.indexOf("a",6));
+ console.log(s.indexOf("a",7));
+ console.log(s.lastIndexOf("a"));
+ let str3=s.slice(4,7);
+ console.log(str3);
+ let str5=s.slice(-7,-4);
+ console.log(str5);
+ console.log((s.substr(5,6)));
