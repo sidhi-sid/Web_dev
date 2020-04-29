e45441a6ae5aafb7101769af9c52fff8b888029e
sidhi-sid <sidhisidh469@gmail.com>
2020-04-30 01:44:55 +0530

    post in express

--- javascript-lectures/routers-eg/routes/students.js
+++ javascript-lectures/routers-eg/routes/students.js
@@ -6,6 +6,14 @@ let students=[
     {name:"d1",college:"IIT-D",year:"III"}
 ];
 route.get('/',(req,res)=>res.send(students));
+route.post('/',(req,res)=>{
+    students.push({
+        name:req.body.name,
+        college:req.body.college,
+        year:req.body.year
+    })
+    res.send(students)
+})
 
 route.get('/add',(req,res)=>{
     students.push({
@@ -23,5 +31,5 @@ route.get('/:id',(req,res)=>res.send(students[req.params.id]));
     //}
     //res.send(students[req.params.id])
     //})
-}
+//}
 module.exports=route;
--- javascript-lectures/routers-eg/server.js
+++ javascript-lectures/routers-eg/server.js
@@ -4,6 +4,10 @@ srv.get('/',function(req,res){
     console.log("you");
     res.send("Hello World");
    });
+srv.use(express.json())
+//a middleware that turns json object in body into JS
+srv.use(express.urlencoded({extended:true}))
+//a middleware decodes a body if it is in url encoded format
 const teacherRoute=require('./routes/teachers');
 const studentRoute=require('./routes/students');
 srv.use('/students',studentRoute);
