423afa3b169fcdbf02f934ca034f52cceb0c929d
sidhi-sid <sidhisidh469@gmail.com>
2020-05-18 23:35:04 +0530

    better version

--- javascript-lectures/mysql_express_server/db.js
+++ javascript-lectures/mysql_express_server/db.js
@@ -1,4 +1,5 @@
 const mysql=require('mysql2')
+
 const connection=mysql.createConnection({
     host:'localhost',
     user:'myuser',
--- javascript-lectures/mysql_express_server/server.js
+++ javascript-lectures/mysql_express_server/server.js
@@ -1,30 +1,14 @@
 const express=require('express')
 const srv=express()
-const db=require('./db')
+const path=require('path')
 srv.use(express.json())
 srv.use(express.urlencoded({extended:true}))
-srv.set("view engine","hbs")
-srv.get('/',(req,res)=>{
-    db.getallpersons()
-        .then((persons)=>{
-            res.render('persons',{persons})
-        })
-        .catch((err)=>{
 
-        })
-})
-srv.get('/add',(req,res)=>{
-    res.render('persons_add')
-})
-srv.post('/add',(req,res)=>{
-    db.addnewperson(req.body.name,req.body.age,req.body.city)
-    .then(()=>{
-        res.redirect('/')
-    })
-    .catch((err)=>{
-        res.send(err)
-    })
-})
+srv.set("view engine","hbs")
+
+srv.use('/pages',require('./routes/pages').route)
+srv.use('/api',require('./routes/api').route)
+srv.use('/',express.static(path.join(__dirname,'public_static')))
 srv.listen(4444,()=>{
     console.log("Server started")
 })
