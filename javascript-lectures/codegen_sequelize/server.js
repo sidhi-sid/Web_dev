189f26f139f6d6f6566e732cd923e4ac4a30c579
sidhi-sid <sidhisidh469@gmail.com>
2020-05-29 00:48:03 +0530

    form submission

--- javascript-lectures/codegen_sequelize/server.js
+++ javascript-lectures/codegen_sequelize/server.js
@@ -1,9 +1,11 @@
 const express=require('express')
 const path=require('path')
-const {center,season,course}=require('./db/models')
+const {center,season,course,batch}=require('./db/models')
 const app=express()
 app.set('view engine','hbs')
 app.set('views',path.resolve(__dirname,'./views'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 app.get('/batchcode',async(req,res)=>{
     try{
         const centers=await center.findAll()
@@ -17,6 +19,31 @@ app.get('/batchcode',async(req,res)=>{
         console.error(e)
     }
    
+})
+app.post('/batchcode',async(req,res)=>{
+    let batchcode='' //WDPP18S1
+    
+    batchcode +=req.body.course
+    batchcode +=req.body.center
+    batchcode +=req.body.year.substr(2)
+    batchcode +=req.body.season
+    batchcode +=req.body.batchno
+    try{
+        const batch = await batch.create({
+            code:batchcode,
+            year:req.body.year,
+            courseId:req.body.course,
+            centerId:req.body.center,
+            seasonId:req.body.season,
+            start:Date.parse(req.body.start),
+            end:Date.parse(req.body.end)
+        })
+       res.send(batch.code)
+    }
+    catch{
+        console.error(e)
+    }
+    
 })
 module.exports={
     app
