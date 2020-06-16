fb45dce3fe9f01585a8fb510dbc42b8d6727f938
sidhi-sid <sidhisidh469@gmail.com>
2020-06-16 22:31:00 +0530

    begin testing

--- javascript-lectures/unit_testing/public/script.js
+++ javascript-lectures/unit_testing/public/script.js
@@ -3,10 +3,31 @@ $(function (){
     let minbox=$('#min')
     let calcbtn=$('#calcfare')
     let farediv=$('#fare')
+    let ratediv=$('#rates')
+    let ratebtn=$('#getrates')
 
     calcbtn.click(function(){
-        $.post('/calcfare',function(data){
-            farediv.innerText=data.fare
+        $.post('/calcfare',{
+            km:kmbox.val(),
+            min:minbox.val()
+        },
+            function(data){
+            farediv.text(`Fare : `+data.fare)
+        })
+    })
+    ratebtn.click(function(){
+        $.get('/rate',function(data){
+            let prettyratedata=`
+            <br>
+            Fixed Fare = Rs. ${data.fixed} for ${data.minkm} KM
+           <br>
+            Fare (Distance) = Rs. ${data.perkm} /KM
+            <br>
+            Fare (Waiting) = Rs. ${data.permin} / min (after ${data.freemin} min)
+            <br>
+
+            `
+            ratediv.html(prettyratedata)
         })
     })
 })
--- javascript-lectures/unit_testing/server.js
+++ javascript-lectures/unit_testing/server.js
@@ -1,18 +1,24 @@
 const express=require('express')
 const path=require('path')
-
+const fareutils=require('./fareutils')
 const app=express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
 app.use('/',express.static(path.join(__dirname,'public')))
-app.listen(2222,()=>console.log('server started'))
+
 
 app.post('/calcfare',(req,res)=>{
     let km=parseFloat(req.body.km)
-    let min=parseInt(req.body.fare)
+    let min=parseInt(req.body.min)
 
-    let fare=50
-    fare+=(km>5)?((km-5)*10):0
-    fare+=(min>15)?((min-15)*2):0
+    let fare=fareutils.calcfare(km,min)
 
     res.send({fare:fare})
 })
+
+app.get('/rate',(req,res)=>{
+    res.send(fareutils.rate)
+})
+app.listen(2222,()=>console.log('server started'))
